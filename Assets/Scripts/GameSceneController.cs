5a5bd258d90aa35b8f177aec0a537dc111150d85
DaddyTrap <914670787@qq.com>
2019-07-06 20:48:08 +0800

    完成表格导入工具，完成根据版面设计生成敌人

--- Assets/Scripts/GameSceneController.cs
+++ Assets/Scripts/GameSceneController.cs
@@ -6,7 +6,7 @@ public class GameSceneController : MonoBehaviour {
     public EnemyPool enemyPool;
     public BulletPool bulletPool;
 
-    public Wave[] waves;
+    public WaveScriptable waveScriptable;
 
     public bool gameRunning { get; set; }
 
@@ -21,24 +21,54 @@ public class GameSceneController : MonoBehaviour {
         gameRunning = false;
     }
 
-    private bool spawned = false;
+    float gameStartTime = 0f;
+    float elapsedTime = 0f;
     void Update() {
-        if (!spawned) {
-            spawned = true;
-            // TODO: 刷新敌人
-            var wave = waves[0];
-            foreach (var waveEle in wave.eles) {
-                GameObject enemy = null;
-                enemy = enemyPool.getOneInstance(waveEle.enemyType, waveEle.isRange);
-                enemy.transform.position = waveEle.startPosition;
-                // 设置 Enemy 的 character
-                enemy.GetComponent<Enemy>().setCharacter(mainCharacter.gameObject);
-            }
+        if (gameRunning) {
+            elapsedTime = Time.time - gameStartTime;
+            // 检测是否该生成下一波怪
+            CheckSpawn();
+        }
+        if (Input.GetKeyDown(KeyCode.F) && !gameRunning) {
+            gameRunning = true;
+            gameStartTime = Time.time;
+            StartGame();
+        }
+    }
+
+    void CheckSpawn() {
+        if (currentWaveIndex + 1 >= waveScriptable.waves.Length) {
+            // 游戏结束
+            gameRunning = false;
+            return;
+        }
+        var nextWave = waveScriptable.waves[currentWaveIndex + 1];
+        if (nextWave.time <= elapsedTime) {
+            Debug.Log(nextWave.time);
+            Debug.Log(currentWaveIndex);
+            // 刷这一波
+            SpawnWave(nextWave);
+            currentWaveIndex = currentWaveIndex + 1;
+            Debug.Log(currentWaveIndex);
+        }
+    }
+
+    private int currentWaveIndex = -1;
+
+    void SpawnWave(WaveScriptable.Wave wave) {
+        foreach (var waveEle in wave.waveEles) {
+            GameObject enemy = null;
+            Debug.Log(waveEle.enemyType);
+            enemy = enemyPool.getOneInstance(waveEle.enemyType, waveEle.isRange);
+            enemy.transform.position = waveScriptable.positions[waveEle.positionIndex];
+            // 设置 Enemy 的 character
+            enemy.GetComponent<Enemy>().setCharacter(mainCharacter.gameObject);
         }
     }
 
     public void StartGame() {
         // TODO: Game Init
+        Debug.Log("GAME START!");
         gameRunning = true;
     }
 }
